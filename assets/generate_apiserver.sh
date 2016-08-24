65b3b78b30f12422264adae508c55c11164550df
Michael Venezia <michael.venezia@viacom.com>
2016-08-24 17:06:51 -0400

    Adding differentiation for client and server etcd

--- assets/generate_apiserver.sh
+++ assets/generate_apiserver.sh
@@ -35,9 +35,11 @@ cp /input/ca.pem /output/kubernetes/ssl/ca.pem
 openssl genrsa -out /output/kubernetes/ssl/apiserver-key.pem 2048
 openssl req -new -key /output/kubernetes/ssl/apiserver-key.pem -out /output/kubernetes/ssl/apiserver.csr -subj "/CN=kube-apiserver" -config /assets/apiserver.conf
 openssl x509 -req -in /output/kubernetes/ssl/apiserver.csr -CA /output/kubernetes/ssl/ca.pem -CAkey /input/ca-key.pem -CAcreateserial -out /output/kubernetes/ssl/apiserver.pem -days 3650 -extensions v3_req -extfile /assets/apiserver.conf
-cp /output/kubernetes/ssl/ca.pem /output/etcd/ssl/ca.pem
-cp /output/kubernetes/ssl/apiserver.pem /output/etcd/ssl/client.pem
-cp /output/kubernetes/ssl/apiserver-key.pem /output/etcd/ssl/client-key.pem
+
+cp /input/ca.pem /output/etcd/ssl/client-ca.pem
+openssl genrsa -out /output/etcd/ssl/client-key.pem 2048
+openssl req -new -key /output/etcd/ssl/client-key.pem -out /output/etcd/ssl/client.csr -subj "/CN=etcd-client" -config /assets/etcd_client.conf
+openssl x509 -req -in /output/etcd/ssl/client.csr -CA /output/etcd/ssl/client-ca.pem -CAkey /input/ca-key.pem -CAcreateserial -out /output/etcd/ssl/client.pem -days 3650 -extensions v3_req -extfile /assets/etcd_client.conf
 
 
 
--- assets/generate_worker.sh
+++ assets/generate_worker.sh
@@ -27,9 +27,11 @@ cp /input/ca.pem /output/kubernetes/ssl/ca.pem
 openssl genrsa -out /output/kubernetes/ssl/worker-key.pem 2048
 openssl req -new -key /output/kubernetes/ssl/worker-key.pem -out /output/kubernetes/ssl/worker.csr -subj "/CN=kube-worker" -config /assets/worker.conf
 openssl x509 -req -in /output/kubernetes/ssl/worker.csr -CA /output/kubernetes/ssl/ca.pem -CAkey /input/ca-key.pem -CAcreateserial -out /output/kubernetes/ssl/worker.pem -days 3650 -extensions v3_req -extfile /assets/worker.conf
-cp /output/kubernetes/ssl/ca.pem /output/etcd/ssl/ca.pem
-cp /output/kubernetes/ssl/worker.pem /output/etcd/ssl/client.pem
-cp /output/kubernetes/ssl/worker-key.pem /output/etcd/ssl/client-key.pem
+
+cp /input/ca.pem /output/etcd/ssl/client-ca.pem
+openssl genrsa -out /output/etcd/ssl/client-key.pem 2048
+openssl req -new -key /output/etcd/ssl/client-key.pem -out /output/etcd/ssl/client.csr -subj "/CN=etcd-client" -config /assets/etcd_client.conf
+openssl x509 -req -in /output/etcd/ssl/client.csr -CA /output/etcd/ssl/client-ca.pem -CAkey /input/ca-key.pem -CAcreateserial -out /output/etcd/ssl/client.pem -days 3650 -extensions v3_req -extfile /assets/etcd_client.conf
 
 
 
