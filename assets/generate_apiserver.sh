a7ec5f12fc2ef8e4098b74b4c19745646ce48117
Michael Venezia <michael.venezia@viacom.com>
2016-09-21 14:24:57 -0400

    Fixing missing directory

--- assets/generate_apiserver.sh
+++ assets/generate_apiserver.sh
@@ -43,7 +43,7 @@ openssl genrsa -out /output/kubernetes/ssl/apiserver-key.pem 2048
 openssl req -new -key /output/kubernetes/ssl/apiserver-key.pem -out /output/kubernetes/ssl/apiserver.csr -subj "/CN=kube-apiserver" -config /assets/apiserver.conf
 openssl x509 -req -in /output/kubernetes/ssl/apiserver.csr -CA /output/kubernetes/ssl/ca.pem -CAkey /input/ca-key.pem -CAcreateserial -out /output/kubernetes/ssl/apiserver.pem -days 3650 -extensions v3_req -extfile /assets/apiserver.conf
 
-cp /input/ca.pem /output/ssl/client-ca.pem
+cp /input/ca.pem /output/etcd/ssl/client-ca.pem
 openssl genrsa -out /output/etcd/ssl/client-key.pem 2048
 openssl req -new -key /output/etcd/ssl/client-key.pem -out /output/etcd/ssl/client.csr -subj "/CN=etcd-client" -config /assets/etcd_client.conf
 openssl x509 -req -in /output/etcd/ssl/client.csr -CA /output/etcd/ssl/client-ca.pem -CAkey /input/ca-key.pem -CAcreateserial -out /output/etcd/ssl/client.pem -days 3650 -extensions v3_req -extfile /assets/etcd_client.conf
